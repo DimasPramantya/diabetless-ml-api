40dd573be599e6bb8098efa5a9ea784c9d30e3cf
DimasPramantya <igkdimas@gmail.com>
2024-06-05 10:27:57 +0700

    specify db connection

--- util/connect_db.js
+++ util/connect_db.js
@@ -1,13 +1,17 @@
+require('dotenv').config();
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
 
 const serviceAccount = require('../firestore-credentials.json')
 
-initializeApp({
+const dbId = process.env.DATABASE_ID
+
+const firestoreApp = initializeApp({
   credential: cert(serviceAccount),
-  projectId: process.env.PROJECT_ID
+  projectId: process.env.PROJECT_ID,
 });
 
-const db = getFirestore();
+const db = getFirestore(firestoreApp, dbId);
+
 
 module.exports = db;
